1701fd2ded97b77feefdaecbe147b0f9f7a7de8e
deathrack <127497796+deathrack@users.noreply.github.com>
2024-02-26 23:36:13 +0530

    ReactRouter

---
+++ 07reactrouter/src/main.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Layout from './Layout'
+import Home from './components/Home/Home'
+import About from './components/About/About'
+import Contact from './components/Contact/Contact'
+import User from './components/User/User'
+import Github from './components/Github/Github'
+
+const router = createBrowserRouter([
+   {
+    path: '/',
+    element: <Layout></Layout>,
+    children:[
+      {
+        path:"",
+        element: <Home></Home>
+      },
+      {
+        path: "about",
+        element: <About/>
+      },
+      {
+        path: "contact",
+        element: <Contact />
+      },
+      {
+        path: "user/:userid",
+        element: <User />
+      },
+      {
+        
+        path: "github",
+        element: <Github />
+      }
+    ]
+   }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}>
+
+    </RouterProvider>
+  </React.StrictMode>,
+)
