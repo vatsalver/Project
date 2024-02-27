b97e586c1b7bd3d5470b554a006b0eb4163938ce
deathrack <127497796+deathrack@users.noreply.github.com>
2024-02-28 01:38:45 +0530

    themeSwitcher

---
+++ 09contextswitcher/src/App.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import { ThemeProvider } from "./contexts/theme";
+import ThemeBtn from "./components/ThemeBtn";
+import Card from "./components/Card";
+
+function App() {
+  const [themeMode, setThememode] = useState("light");
+  const lightTheme = () => {
+    setThememode("light");
+  };
+  const darkTheme = () => {
+    setThememode("dark");
+  };
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+
+  return (
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
+          <ThemeBtn />
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
