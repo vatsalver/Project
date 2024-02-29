32e13f440ba339febff0cda033726d882593c51f
deathrack <127497796+deathrack@users.noreply.github.com>
2024-02-29 22:06:41 +0530

    redux todo

---
+++ reduxtoolkittodo/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
+
+function App() {
+  return (
+    <>
+      <h1>Learn about redux Toolkit</h1>
+      <AddTodo></AddTodo>
+      <Todos></Todos>
+    </>
+  );
+}
+
+export default App;
---
+++ reduxtoolkittodo/src/components/Todos.jsx
@@ -0,0 +1,20 @@
+import { useSelector, useDispatch } from "react-redux";
+import { removeTodo } from "../features/todo/todoSlice";
+
+function Todos() {
+  const todos = useSelector((state) => state.todos);
+  const dispacth = useDispatch();
+  return (
+    <>
+      <div>Todos</div>
+      {todos.map((todo) => (
+        <li key={todo.id}>
+          {todo.text}
+          <button onClick={() => dispacth(removeTodo(todo.id))}>X</button>
+        </li>
+      ))}
+    </>
+  );
+}
+
+export default Todos;
