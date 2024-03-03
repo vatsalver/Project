f03c69743538487401aec67dfa73e16384732b5e
deathrack <127497796+deathrack@users.noreply.github.com>
2024-03-03 16:03:52 +0530

    reduxtoolkit

--- 12appwriteBlog/src/App.jsx
+++ 12appwriteBlog/src/App.jsx
@@ -1,12 +1,38 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import "./App.css";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <>
-      <h1>Hello WORLD !</h1>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header></Header>
+        <main>// Outlet</main>
+        <Footer></Footer>
+      </div>
+      Hello
+    </div>
+  ) : null;
 }
 
 export default App;
---
+++ 12appwriteBlog/src/components/Footer/Footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <div>Hello</div>;
+};
+
+export default Footer;
