eb408bd6304d4381827d57e07e88f8902d2416fa
deathrack <127497796+deathrack@users.noreply.github.com>
2024-03-04 23:23:56 +0530

    added RTE (editor)

---
+++ 12appwriteBlog/src/components/RTE.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Editor } from "@tinymce/tinymce-react";
+import { Controller } from "react-hook-form";
+
+function RTE({ name, control, label, defaultValue = "" }) {
+  return (
+    <div className="w-full">
+      {label && <label className="inline-block mb-1 pl-1">{label}</label>}
+      <Controller
+        name={name || "content"}
+        control={control}
+        render={({ field: { onChange } }) => (
+          <Editor
+            initialValue="default value"
+            init={{
+              branding: false,
+              height: 500,
+              menubar: true,
+              plugins: [
+                "advlist autolink lists link image charmap print preview anchor",
+                "searchreplace visualblocks code fullscreen",
+                "insertdatetime media table paste code help wordcount",
+              ],
+              toolbar:
+                "undo redo | format select | bold italic underline | \
+           alignleft aligncenter alignright alignjustify | \
+           bullist numlist outdent indent | removeformat | help",
+              content_style:
+                "body {font-family:Helvetica,Arial,sans-serif;font-size:14px}",
+            }}
+            onEditorChange={onChange}
+          ></Editor>
+        )}
+      ></Controller>
+    </div>
+  );
+}
+
+export default RTE;
