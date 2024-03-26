6559e1a1bf0b1e93a22e18bb4bbcdfbb4f03d7cd
deathrack <127497796+deathrack@users.noreply.github.com>
2024-03-27 00:10:54 +0530

    Navbar final

--- musicapp/src/app/page.tsx
+++ musicapp/src/app/page.tsx
@@ -1,5 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <>Vatsal</>;
+  return (
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <h1 className="text-2xl text-center">Vatsal</h1>
+    </main>
+  );
 }
