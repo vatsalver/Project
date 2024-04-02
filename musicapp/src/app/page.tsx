868dda9e3ba5797aecaf21b920049800b90ad23f
deathrack <127497796+deathrack@users.noreply.github.com>
2024-04-02 22:27:22 +0530

    link

--- musicapp/src/app/page.tsx
+++ musicapp/src/app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/HeroSection";
 import Card from "@/components/Card";
+import { HoveredLink } from "@/components/ui/navbar-menu";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02] bg-transparent">
       <HeroSection />
       <Card></Card>
+      <HoveredLink></HoveredLink>
     </main>
   );
 }
