e296354453e8c8d013602089df0c37baaf9b8de8
deathrack <127497796+deathrack@users.noreply.github.com>
2024-04-12 23:43:48 +0530

    Register/Login Pages

--- bookingapp/client/src/App.jsx
+++ bookingapp/client/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import IndexPage from "./pages/IndexPage";
 import LoginPage from "./pages/LoginPage";
 import Layout from "./Layout";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<IndexPage />}></Route>
           <Route path="/login" element={<LoginPage />}></Route>
+          <Route path="/register" element={<RegisterPage />}></Route>
         </Route>
       </Routes>
     </>
--- bookingapp/client/src/pages/RegisterPage.jsx
+++ bookingapp/client/src/pages/RegisterPage.jsx
@@ -1,19 +1,20 @@
 import { Link } from "react-router-dom";
 
-const LoginPage = () => {
+function RegisterPage() {
   return (
     <>
       <div className="mt-4 grow flex items-center justify-around">
         <div className="mb-64">
-          <h1 className="text-4xl text-center mb-4">Login</h1>
+          <h1 className="text-4xl text-center mb-4">Register</h1>
           <form className="max-w-md mx-auto">
+            <input type="text" placeholder="Full name"></input>
             <input type="email" placeholder="your@email.com"></input>
             <input type="password" placeholder="password"></input>
             <button className="primary">Login</button>
             <div className="text-center py-2 text-gray-500">
-              Don&apos;t Have an account yet?
-              <Link className="underline text-black p-2" to={"/register"}>
-                Register Now
+              Already Have an account ?
+              <Link className="underline text-black p-2" to={"/login"}>
+                Login Here
               </Link>
             </div>
           </form>
@@ -21,6 +22,6 @@ const LoginPage = () => {
       </div>
     </>
   );
-};
+}
 
-export default LoginPage;
+export default RegisterPage;
