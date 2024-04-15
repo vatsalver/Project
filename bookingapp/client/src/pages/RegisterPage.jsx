9cea3dd2d24af251f4bde2864ff620618d3065a3
deathrack <127497796+vatsalver@users.noreply.github.com>
2024-04-15 23:49:50 +0530

    Function

--- bookingapp/client/src/pages/RegisterPage.jsx
+++ bookingapp/client/src/pages/RegisterPage.jsx
@@ -1,15 +1,35 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function RegisterPage() {
+  const [name, setName] = useState("");
+  const [email, setemail] = useState("");
+  const [password, setPassword] = useState("");
+  function registerUser() {}
   return (
     <>
       <div className="mt-4 grow flex items-center justify-around">
         <div className="mb-64">
           <h1 className="text-4xl text-center mb-4">Register</h1>
-          <form className="max-w-md mx-auto">
-            <input type="text" placeholder="Full name"></input>
-            <input type="email" placeholder="your@email.com"></input>
-            <input type="password" placeholder="password"></input>
+          <form className="max-w-md mx-auto" onSubmit={registerUser}>
+            <input
+              type="text"
+              placeholder="Full name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            ></input>
+            <input
+              type="email"
+              placeholder="your@email.com"
+              value={email}
+              onChange={(e) => setemail(e.target.value)}
+            ></input>
+            <input
+              type="password"
+              placeholder="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></input>
             <button className="primary">Login</button>
             <div className="text-center py-2 text-gray-500">
               Already Have an account ?
